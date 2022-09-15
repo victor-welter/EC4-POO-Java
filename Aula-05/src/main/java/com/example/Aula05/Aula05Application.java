c760799d5ba4e9304414f26fb3e2b42ce99da9ff
Víctor Welter <84386775+victor-welter@users.noreply.github.com>
2022-09-15 18:58:22 -0300

    Adicionado exemplo aula 05

---
+++ Aula-05/src/main/java/com/example/Aula05/Aula05Application.java
@@ -0,0 +1,13 @@
+package com.example.Aula05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aula05Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aula05Application.class, args);
+	}
+
+}
---
+++ Aula-05/src/test/java/com/example/Aula05/Aula05ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Aula05;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aula05ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
